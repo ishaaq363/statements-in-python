e25536a5870a5d167e4eef94964e13be175e2ee4
ishaaq363 <imatovu383@gmail.com>
2024-12-14 18:23:57 +0200

    statement

---
+++ main.py
@@ -0,0 +1,14 @@
+# age = (input("enter your age sir/madam"))
+
+# if age>18:
+#     print("eligible for votting")
+
+# else:
+#      print("not eligible for votting ")
+
+number = int(input(" enter number from 1 to 50"))
+if number>0:
+       print("negative")
+
+else:  
+    print("positive")   
